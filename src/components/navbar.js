05dfe2a5d057516b2bbb1dd11c026febde128303
FurqanAthar <22100070@lums.edu.pk>
2021-04-10 00:36:55 +0500

    navbar changed
    added navlinks and changed the css a bit

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -6,39 +6,31 @@ import { faShoppingCart, faUser, faBars } from '@fortawesome/free-solid-svg-icon
 
 export default function Navbar() {
     return (
-        <div>
+        <div id="myHeader">
             <nav>
-                <img src={Logo} alt="Techyard" className="logo"/>
+                <Link to='/'><img src={Logo} alt="Techyard" className="logo"/></Link>
                 <form>
                     <input type="text" placeholder="Search..."/>
                 </form>
                 <ul className="mainMenu">
                     <li><Link to='/'><FontAwesomeIcon icon={faShoppingCart}></FontAwesomeIcon></Link></li>
                     <li><Link to='/'><FontAwesomeIcon icon={faUser}></FontAwesomeIcon></Link></li>
-                    <li>
-                        <input type="checkbox" id="toggle"/>
-                        <label htmlFor="toggle">
-                            <FontAwesomeIcon icon={faBars}></FontAwesomeIcon>
-                        </label>
-                    </li>
                 </ul>
                 <ul className="sidebar">
                     <li><Link to='/'><FontAwesomeIcon icon={faBars}></FontAwesomeIcon></Link></li>
                 </ul>
-                <div id="slidingMenu">
-                    <ul>
-                        <li>
-                            <Link to='/'>Home</Link>
-                        </li>
-                        <li>
-                            <Link to='/'>Home</Link>
-                        </li>
-                        <li>
-                            <Link to='/'>Home</Link>
-                        </li>
-                    </ul>
-                </div>
             </nav>
+            <ul className="navLinks">
+                <li>
+                    <Link to='/'>Home</Link>
+                </li>
+                <li>
+                    <Link to='/mobiles'>Mobiles</Link>
+                </li>
+                <li>
+                    <Link to='/'>Home</Link>
+                </li>
+            </ul>
         </div>
     )
 }
